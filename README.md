a199dad2c87986c3f1a31c4c1a3665413be6e3f0
CloverCLC <cloverclc@outlook.com>
2026-04-24 16:54:38 +0800

    修复LLM返回

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-uv sync 初始化项目
+# MCP学习笔记
+#### 项目初始化
+```
+uv sync
+```
+## 在项目中遇到的难点
+- MCP Host 和 MCP server之间的对话
+我们需要引入 re (正则库) 来解析 XML
+- MCP server 会话管理
+- 
--- host/host.py
+++ host/host.py
@@ -3,6 +3,7 @@ from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 import asyncio
+import re
 import json
 from pathlib import Path
 from mcp.client.stdio import stdio_client, StdioServerParameters
@@ -115,35 +116,12 @@ class MCPClientManager:
             
             # 把所有工具的文本用换行符拼成一个超级长的字符串
             tools_string = "\n".join(tools_str_list)
-
+            sys_prompt_path = Path(SYSTEM_PROMPT_PATH)
             # 将工具字符串拼接到系统提示词中
+            with open(sys_prompt_path, "r", encoding="utf-8") as f:
+                sys_prompt = f.read()
             final_system_prompt = f"""
-你是 Mkils，一位技艺高超的软件工程师，精通多种编程语言、框架、设计模式和最佳实践。
-
-====
-
-工具使用
-
-你可以访问一组工具，这些工具在用户批准后执行。每条消息可以使用一个工具，并且将在用户的响应中收到该工具使用的结果。你可以逐步使用工具来完成给定任务，每个工具的使用都基于前一个工具使用结果。
-
-# 工具使用格式
-
-工具使用采用 XML 风格的标签进行格式化。工具名称包含在开始和结束标签中，每个参数也同样包含在其自己的一组标签中。结构如下：
-
-<tool_name>
-<parameter1_name>value1</parameter1_name>
-<parameter2_name>value2</parameter2_name>
-...
-</tool_name>
-
-例如：
-
-<read_file>
-<path>src/main.js</path>
-</read_file>
-
-始终遵守此格式以确保工具使用的正确解析和执行。
-
+{sys_prompt}
 #可用工具列表
 {tools_string}
             """
